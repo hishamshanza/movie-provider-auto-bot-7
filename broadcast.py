5bd37a65b64700fbb5eeaee616caf3cfac30b883
hishamshanza <nusrathchelat@gmail.com>
2026-01-22 23:34:45 +0530

    Add files via upload

--- broadcast.py
+++ broadcast.py
@@ -2,34 +2,28 @@
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-# Clone Code Credit : YT - @Tech_VJ / TG - @VJ_Bots / GitHub - @VJBots
-
 import datetime, time, asyncio
 from pyrogram import Client, filters
 from database.users_chats_db import db
-from pyrogram.errors import *
-from CloneTechVJ.database.clone_bot_userdb import clonedb
+from info import ADMINS
+from utils import broadcast_messages, broadcast_messages_group
         
-@Client.on_message(filters.command("broadcast"))
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS))
 async def pm_broadcast(bot, message):
-    me = await bot.get_me()
-    owner = await db.get_bot(me.id)
-    if owner["user_id"] != message.from_user.id:
-        return 
     b_msg = await bot.ask(chat_id = message.from_user.id, text = "Now Send Me Your Broadcast Message")
     try:
-        users = await clonedb.get_all_users(me.id)
+        users = await db.get_all_users()
         sts = await message.reply_text('Broadcasting your messages...')
         start_time = time.time()
-        total_users = await clonedb.total_users_count(me.id)
+        total_users = await db.total_users_count()
         done = 0
         blocked = 0
         deleted = 0
         failed = 0
         success = 0
         async for user in users:
-            if 'user_id' in user:
-                pti, sh = await broadcast_messages(me.id, int(user['user_id']), b_msg)
+            if 'id' in user:
+                pti, sh = await broadcast_messages(int(user['id']), b_msg)
                 if pti:
                     success += 1
                 elif pti == False:
@@ -58,22 +52,30 @@ async def pm_broadcast(bot, message):
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-async def broadcast_messages(bot_id, user_id, message):
-    try:
-        await message.copy(chat_id=user_id)
-        return True, "Success"
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-        return await broadcast_messages(bot_id, user_id, message)
-    except InputUserDeactivated:
-        await clonedb.delete_user(bot_id, user_id)
-        return False, "Deleted"
-    except UserIsBlocked:
-        await clonedb.delete_user(bot_id, user_id)
-        return False, "Blocked"
-    except PeerIdInvalid:
-        await clonedb.delete_user(bot_id, user_id)
-        return False, "Error"
-    except Exception as e:
-        await clonedb.delete_user(bot_id, user_id)
-        return False, "Error"
+@Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS))
+async def broadcast_group(bot, message):
+    b_msg = await bot.ask(chat_id = message.from_user.id, text = "Now Send Me Your Broadcast Message")
+    groups = await db.get_all_chats()
+    sts = await message.reply_text(
+        text='Broadcasting your messages To Groups...'
+    )
+    start_time = time.time()
+    total_groups = await db.total_chat_count()
+    done = 0
+    failed = 0
+
+    success = 0
+    async for group in groups:
+        pti, sh = await broadcast_messages_group(int(group['id']), b_msg)
+        if pti:
+            success += 1
+        elif sh == "Error":
+                failed += 1
+        done += 1
+        if not done % 20:
+            await sts.edit(f"Broadcast in progress:\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}")    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}")
+        
+
+
